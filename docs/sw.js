f090965f83a32b9f15fc6286bff3eda3714c2cf8
Mauro Solcia <mauro.solcia@gmail.com>
2023-11-17 22:49:32 +0100

    sw.js v3

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/ESP32_Camper';
 var APP_PREFIX = 'esp32_camper_';
-var VERSION = 'version_002';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,

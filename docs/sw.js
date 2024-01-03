786671e81cfe733a80e5781ecd080bb8609da8e3
Mauro Solcia <mauro.solcia@gmail.com>
2024-01-03 20:56:54 +0100

    Open Site from notification

--- docs/sw.js
+++ docs/sw.js
@@ -5,7 +5,7 @@ if (location.href.startsWith('http://localhost')) {
 }
 var GHPATH = base;
 var APP_PREFIX = 'esp32_camper_';
-var VERSION = 'version_019';
+var VERSION = 'version_020';
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
@@ -84,4 +84,28 @@ self.addEventListener('message', function (evt) {
     );
   }
 
+})
+
+self.addEventListener('notificationclick', function (event) {
+  let url = self.location.origin + base;
+  if (location.href.startsWith('http://localhost')) {
+    url += "/index.html";
+  }
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        // If so, just focus it.
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 })

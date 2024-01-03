0bd145ecde104b9b196ccd4c9d318c1c2c6edba7
Mauro Solcia <mauro.solcia@gmail.com>
2024-01-03 21:01:08 +0100

    Version change

--- docs/sw.js
+++ docs/sw.js
@@ -5,7 +5,7 @@ if (location.href.startsWith('http://localhost')) {
 }
 var GHPATH = base;
 var APP_PREFIX = 'esp32_camper_';
-var VERSION = 'version_020';
+var VERSION = 'version_021';
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,

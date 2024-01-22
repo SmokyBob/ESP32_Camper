2a3722051f326887065fbbd05c85d62fcd13c2b7
Mauro Solcia <mauro.solcia@gmail.com>
2024-01-22 20:22:46 +0100

    Disable notification button css Touch friendly style for the button

--- docs/sw.js
+++ docs/sw.js
@@ -5,7 +5,7 @@ if (location.href.startsWith('http://localhost')) {
 }
 var GHPATH = base;
 var APP_PREFIX = 'esp32_camper_';
-var VERSION = 'version_023';
+var VERSION = 'version_024';
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
